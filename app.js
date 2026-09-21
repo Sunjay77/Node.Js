bc57a7d856bab8370af08518ff903ebb63226d21
Sunjay77 <sanjaylamaa77@gmail.com>
2026-09-21 15:32:17 +0545

    Add comments explaining Node.js global objects and module system

--- app.js
+++ app.js
@@ -3,3 +3,20 @@
 // }
 // // sayHello( " sunjay" );
 // // console.log(window); // in node we dont have window or document objects
+
+// Node Module System
+
+// console.log(); //global
+
+// setTimeout();
+// clearTimeout();
+
+// setInterval();
+// clearInterval();
+
+// window.console.log; // we can use this window object for above all
+
+// var message = ""; // for window var is available but not in global object
+// global.console.log; // instead of window we have global object in node
+// console.log(global.message) // It sends undefined in the terminal
+
