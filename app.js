7be0e405e8bb8fb0e51179a7072fc8e469978fdf
Sunjay77 <sanjaylamaa77@gmail.com>
2026-09-24 13:37:22 +0545

    Comment out event emitter implementation in app.js and add basic HTTP server functionality

--- app.js
+++ app.js
@@ -58,15 +58,31 @@
 
 //Events Module
 
-const EventEmmiter = require(`events`); // class
+// const EventEmmiter = require(`events`); // class
 // const emmiter = new EventEmmiter(); // Actual Object
 
-const Logger = require("./logger");
-const logger = new Logger();
-// Register a listener
-logger.on("messageLogged", (arg) => {
-  // some use e, eventArg
-  console.log("Listener called", arg);
+// const Logger = require("./logger");
+// const logger = new Logger();
+// // Register a listener
+// logger.on("messageLogged", (arg) => {
+//   // some use e, eventArg
+//   console.log("Listener called", arg);
+// });
+
+// logger.log("message");
+
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.write("Hello World");
+    res.end();
+  }
+  if (req.url === "/api/courses") {
+    res.write(JSON.stringify([1, 2, 3]));
+    res.end();
+  }
 });
 
-logger.log("message");
+server.listen(3000);
+console.log("Listening on port 3000...");
